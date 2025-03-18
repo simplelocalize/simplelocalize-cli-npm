b68eb62a8f1af16ad43a6abd25b1a38232ccb0de
Jakub Pomykała <jakub.pomykala@gmail.com>
2025-03-18 15:45:32 +0100

    Init

---
+++ index.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require('fs');
+const axios = require('axios');
+const os = require('os');
+
+const binaryPath = path.join(__dirname, 'bin', 'simplelocalize');
+
+const isBinaryInstalled = () => {
+    try {
+        execSync(binaryPath + ' --version', { stdio: 'ignore' });
+        return true;
+    }
+    catch (error) {
+        return false;
+    }
+}
+
+const printBinaryVersion = () => {
+    try {
+        console.log(execSync(binaryPath + ' --version').toString());
+    }
+    catch (error) {
+        console.error('Error running SimpleLocalize CLI');
+        process.exit(1);
+    }
+}
+
+const buildDownloadBinaryUrl = (version) => {
+    let platform = "";
+    if (os.platform() === "win32") {
+        platform = "windows.exe"
+    }
+
+    if (os.platform() === "darwin") {
+        platform = "mac"
+    }
+
+    if (os.platform() === "linux") {
+        platform = "linux"
+    }
+
+    let arch = "";
+    if (os.arch() === "arm64") {
+        arch = "-arm64"
+    }
+    return `https://github.com/simplelocalize/simplelocalize-cli/releases/download/${version}/simplelocalize-cli-${platform}${arch}`
+}
+
+async function installBinary(version = "2.8.0") {
+    const downloadUrl = buildDownloadBinaryUrl(version);
+    if (!fs.existsSync(path.join(__dirname, 'bin'))) {
+        fs.mkdirSync(path.join(__dirname, 'bin'));
+    }
+    console.log(`Downloading ${downloadUrl}...`);
+    const response = await axios({
+        url: downloadUrl,
+        method: 'GET',
+        responseType: 'stream'
+    });
+
+    const writer = fs.createWriteStream(binaryPath);
+    response.data.pipe(writer);
+    await new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
+
+    console.log('SimpleLocalize CLI downloaded successfully!');
+
+    // Make it executable (only on Unix-based systems)
+    if (os.platform() !== 'win32') {
+        fs.chmodSync(binaryPath, 0o755);
+    }
+
+    console.log('Binary is now executable.');
+}
+
+const init = async () => {
+    console.log("Checking if SimpleLocalize CLI is installed...");
+    if (!isBinaryInstalled()) {
+        console.log("SimpleLocalize CLI not installed. Installing...");
+        await installBinary();
+        if (!isBinaryInstalled()) {
+            console.error('Error installing SimpleLocalize CLI');
+            process.exit(1);
+        }
+        console.log("SimpleLocalize CLI installed successfully!");
+    } else {
+        console.log("SimpleLocalize CLI already installed.");
+    }
+    printBinaryVersion();
+    process.exit(0);
+}
+
+await init();
